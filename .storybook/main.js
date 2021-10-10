e288a25cbd8c34939efbe7db5dee99216caf4b0a
Michael Shilman <michael@lab80.co>
2021-10-10 17:19:15 +0800

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,7 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-react-native-web/preset',
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/addon-react-native-web', '@storybook/addon-essentials'],
   features: {
     babelModeV7: false,
   },
